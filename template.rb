b410f9c8303428ab4df522c98398e8e91adab5b4
Selfrot <selfrotlol@gmail.com>
2014-01-19 02:45:54 +0100

    Append the correct SASS @import to application.css.scss

--- template.rb
+++ template.rb
@@ -6,3 +6,13 @@ Add_Scss::get_input
 
 puts 'Do you want to add the Foundation gem? (Y/N)'
 Add_Foundation::get_input
+
+if File.exist?('app/assets/stylesheets/application.css.scss')
+	insert_into_file 'app/assets/stylesheets/application.css.scss',
+	                 "\n*/\n@import 'foundation_and_overrides';\n/*",
+	                 :after =>  '*= require_self'
+
+	puts 'Added foundation_and_overrides to your application.css.scss file.'
+else
+	puts 'Skipping Scss support for Foundation install.'
+end
