a29108eb8d4574827e277a397c98ded0242c673f
Selfrot <selfrotlol@gmail.com>
2014-01-24 20:35:30 +0100

    Removed the unecessary log

--- util_classes.rb
+++ util_classes.rb
@@ -1,6 +1,5 @@
 class Utilities
 	# Setting Paths
-	LOGFILE = File.join(File.dirname(__FILE__), '/log.txt')
 	TMPFILE = File.join(File.dirname(__FILE__), '/tmp.txt')
 
 	def self.initialize
@@ -9,18 +8,9 @@ class Utilities
 			File.open(file, 'w+') { |tempentry| tempentry.write('') }
 		end
 		end
-
-		unless File.exist?(Utilities::LOGFILE) do |file|
-			File.open(file, 'w+') { |logentry| logentry.write('') }
-		end
-		end
 	end
 
 	class Writes
-
-		def self.log_entry entry
-			File.open(Utilities::LOGFILE, 'a') { |logentry| logentry.write(entry) }
-		end
 
 		def self.tmp_entry entry
 			File.open(Utilities::TMPFILE, 'w+') { |logentry| logentry.write(entry) }
@@ -29,11 +19,6 @@ class Utilities
 	end
 
 	class Reads
-
-		def self.read_log match
-			log_contents = File.open(Utilities::LOGFILE, 'r')
-			(!!(log_contents.read =~ /#{match}/) == true) ? true : false
-		end
 
 		def self.read_tmp match
 			tmp_contents = File.open(Utilities::TMPFILE, 'r')
